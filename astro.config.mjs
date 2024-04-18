f5cbde895c8d0c3a1782ae3aa5950e3a59eaf6bd
nine96as <nine96as@proton.me>
2024-04-18 19:44:14 +0100

    Add mdx integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-      nesting: true,
-    }),
-  ],
-})
+  integrations: [tailwind({
+    applyBaseStyles: false,
+    nesting: true
+  }), mdx()]
+});
