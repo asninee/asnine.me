d76cdfcee3b66fee2519e0162bc043fb14f7b99d
nine96as <nine96as@proton.me>
2024-04-18 17:35:27 +0100

    Clean up config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
-import { defineConfig } from 'astro/config';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+      nesting: true,
+    }),
+  ],
+})
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,73 @@
+import tailwindColors from 'tailwindcss/colors'
+import plugin from 'tailwindcss/plugin'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  darkMode: 'class',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    fontFamily: {
+      sans: [
+        '"Helvetica Neue"',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: [
+        '"Brygada 1918"',
+        'ui-serif',
+        'Georgia',
+        'Cambria',
+        '"Times New Roman"',
+        'serif',
+      ],
+      mono: [
+        'Fira Code',
+        'ui-monospace',
+        'SFMono-Regular',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        'monospace',
+      ],
+    },
+    colors: {
+      inherit: tailwindColors.inherit,
+      current: tailwindColors.current,
+      transparent: tailwindColors.transparent,
+      black: tailwindColors.black,
+      white: tailwindColors.white,
+      gray: tailwindColors.neutral,
+    },
+    extend: {
+      maxWidth: {
+        container: '43rem',
+      },
+      animation: {
+        'scale-up': 'scale-up 150ms ease-in',
+      },
+      keyframes: {
+        'scale-up': {
+          '0%': { transform: 'scale(0.9)', opacity: 0 },
+          '70%': { transform: 'scale(1.02)', opacity: 0.7 },
+          '100%': { transform: 'scale(1)', opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [
+    plugin(({ addVariant }) => {
+      addVariant('selected', '&[aria-selected="true"]')
+      addVariant('current', '&[aria-current="true"]')
+    }),
+  ],
 }
