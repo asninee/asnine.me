2f8b8236274ed1aa033d4d61c1138753a1ef472f
asninee <nine96as@proton.me>
2024-04-22 15:49:09 +0100

    Add shiki config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,30 @@
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false,
-    nesting: true
-  }), mdx()]
-});
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+      nesting: true,
+    }),
+    mdx(),
+  ],
+  markdown: {
+    shikiConfig: {
+      // https://shiki.style/guide/dual-themes
+      themes: {
+        light: 'min-light',
+        dark: 'min-dark',
+      },
+      // https://shiki.style/languages
+      langs: ['js', 'rs'],
+      wrap: true,
+      // Add custom transformers: https://shiki.style/guide/transformers
+      // Find common transformers: https://shiki.style/packages/transformers
+      transformers: [],
+    },
+  },
+})
