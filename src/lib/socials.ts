e04dd76bd9e9ce06f1387c854220e30f7775777a
asninee <nine96as@proton.me>
2024-04-28 17:37:07 +0100

    Add getSocials logic

---
+++ src/lib/socials.ts
@@ -0,0 +1,19 @@
+import { z } from 'astro/zod'
+
+const socialSchema = z.object({
+  name: z.string(),
+  link: z.string(),
+})
+
+export type Social = z.infer<typeof socialSchema>
+
+export const getSocials = async (): Promise<Social[]> => [
+  {
+    name: 'github',
+    link: 'https://github.com/asninee',
+  },
+  {
+    name: 'linkedin',
+    link: 'https://www.linkedin.com/in/asnine',
+  },
+]
