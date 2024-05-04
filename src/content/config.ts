6884141263d76c88727471cfb361f654c7c540dd
asninee <nine96as@proton.me>
2024-05-04 18:52:35 +0100

    Add isDraft to blog schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,6 +3,7 @@ import { defineCollection, z } from 'astro:content'
 const blog = defineCollection({
   type: 'content',
   schema: z.object({
+    isDraft: z.boolean().optional().default(false),
     title: z.string(),
     desc: z.string(),
     date: z.date(),
