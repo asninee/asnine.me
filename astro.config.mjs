79541f790d90b72f6815489c3b4fadaa3294d4a8
asninee <nine96as@proton.me>
2024-05-04 19:54:53 +0100

    Add sitemap integration, add site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import { defineConfig } from 'astro/config'
-
 import icon from 'astro-icon'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://asnine.me',
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     mdx(),
     icon(),
+    sitemap(),
   ],
   markdown: {
     shikiConfig: {
