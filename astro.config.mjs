5bd9e2378c6271f40a4dc597f7aba44d883453ec
asninee <nine96as@proton.me>
2024-05-06 20:21:06 +0100

    Rm cloudfare pkg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import cloudflare from '@astrojs/cloudflare'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
@@ -8,8 +7,6 @@ import { defineConfig } from 'astro/config'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://asnine.me',
-  output: 'hybrid',
-  adapter: cloudflare(),
   integrations: [
     tailwind({
       applyBaseStyles: false,
