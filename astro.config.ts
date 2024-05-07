8de869892ee0d8a5e6823bd05c16cea78acf2e25
asninee <nine96as@proton.me>
2024-05-07 15:26:54 +0100

    Clean up config file

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,9 @@
+import cloudflare from '@astrojs/cloudflare'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
-
-import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
@@ -21,13 +20,10 @@ export default defineConfig({
   ],
   markdown: {
     shikiConfig: {
-      // https://shiki.style/guide/dual-themes
       themes: {
         light: 'github-light',
         dark: 'github-dark',
       },
-      // https://shiki.style/languages
-      langs: ['rs', 'go', 'astro'],
       wrap: false,
     },
   },
