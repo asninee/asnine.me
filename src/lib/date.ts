d0aa9a8cdc2aaacb6368db1176a907d1f7f195fc
asninee <nine96as@proton.me>
2024-05-09 20:19:52 +0100

    Add sortNotesByDate logic

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,4 +1,7 @@
 import type { CollectionEntry } from 'astro:content'
 
-export const sortPostsByDate = (posts: CollectionEntry<'blog'>[]) =>
-  posts.sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
+export const sortPostsByDate = (entries: CollectionEntry<'blog'>[]) =>
+  entries.sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
+
+export const sortNotesByDate = (entries: CollectionEntry<'notes'>[]) =>
+  entries.sort((a, b) => b.data.date.getTime() - a.data.date.getTime())
