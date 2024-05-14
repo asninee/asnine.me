bb26174aae4ef2d508238f2915b7dbb3ffdaccdb
asninee <nine96as@proton.me>
2024-05-14 19:52:05 +0100

    Add Nacelle font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,7 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily: {
-      sans: ['"Helvetica Neue"', ...defaultTheme.fontFamily.sans],
+      sans: ['Nacelle', ...defaultTheme.fontFamily.sans],
       serif: ['Newsreader', ...defaultTheme.fontFamily.serif],
       mono: ['Commit Mono', ...defaultTheme.fontFamily.mono],
     },
