00312b4815bb060e5b419c4a32862c9d7436f299
asninee <nine96as@proton.me>
2024-05-14 19:49:27 +0100

    Rm config comment

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,6 @@ import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://asnine.me',
   output: 'hybrid',
