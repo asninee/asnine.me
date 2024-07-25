2363685bdb10c89b39721a0b75d9d0013b09c202
asninee <nine96as@proton.me>
2024-07-25 16:53:12 +0100

    Default font experiment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,15 @@
-import defaultTheme from 'tailwindcss/defaultTheme'
+// import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: 'selector',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    fontFamily: {
-      sans: ['BDOGrotesk', ...defaultTheme.fontFamily.sans],
-      serif: ['Newsreader', ...defaultTheme.fontFamily.serif],
-      mono: ['Iosevka', ...defaultTheme.fontFamily.mono],
-    },
+    // fontFamily: {
+    //   sans: ['BDOGrotesk', ...defaultTheme.fontFamily.sans],
+    //   serif: ['Newsreader', ...defaultTheme.fontFamily.serif],
+    //   mono: ['Iosevka', ...defaultTheme.fontFamily.mono],
+    // },
   },
   plugins: [],
 }
