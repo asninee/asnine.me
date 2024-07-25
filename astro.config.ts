ef3b2b517d22c8cc43017d70e9fff1e0617956aa
asninee <nine96as@proton.me>
2024-07-25 12:05:22 +0100

    Add playform/compress experiment pt2

--- astro.config.ts
+++ astro.config.ts
@@ -1,20 +1,21 @@
-import mdx from '@astrojs/mdx'
-import sitemap from '@astrojs/sitemap'
-import tailwind from '@astrojs/tailwind'
-import icon from 'astro-icon'
-import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://asnine.me',
-  integrations: [tailwind(), mdx(), icon(), sitemap()],
+  integrations: [tailwind(), mdx(), icon(), sitemap(), playformCompress()],
   markdown: {
     shikiConfig: {
       themes: {
         light: 'github-light',
-        dark: 'github-dark',
+        dark: 'github-dark'
       },
-      wrap: false,
-    },
-  },
-})
+      wrap: false
+    }
+  }
+});
