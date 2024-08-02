ca66a002ce7a5b965df37e8c376ee10df982e53f
asninee <nine96as@proton.me>
2024-08-02 21:02:54 +0100

    Rm icons; no more theme toggle icon

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,12 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://asnine.me',
-  integrations: [tailwind(), mdx(), icon(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       themes: {
