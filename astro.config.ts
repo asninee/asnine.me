6d91d1c348ab13cd5fba391d55bfd184acd76f3f
asninee <nine96as@proton.me>
2024-08-02 21:05:51 +0100

    Add back astro-icons; silly mistake

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,13 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://asnine.me',
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
   markdown: {
     shikiConfig: {
       themes: {
