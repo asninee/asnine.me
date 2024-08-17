93e4116e2beffab4564d3908330e45be0ad0f2cf
asninee <nine96as@proton.me>
2024-08-18 00:11:53 +0100

    Add compress pkg

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,22 @@
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
+import playformCompress from '@playform/compress'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://asnine.me',
-  integrations: [tailwind(), mdx(), sitemap(), icon()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    icon(),
+    playformCompress({
+      CSS: false,
+    }),
+  ],
   markdown: {
     shikiConfig: {
       themes: {
