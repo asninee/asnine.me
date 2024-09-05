073a0bbe3624c61d3a06f3ab977fb08e4b3d4a6c
asninee <nine96as@proton.me>
2024-09-05 11:39:42 +0100

    refactor: rm unnecessary file exts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,md,mdx,ts}'],
   theme: {
     fontFamily: {
       sans: ['OpeningHoursSans', ...defaultTheme.fontFamily.sans],
