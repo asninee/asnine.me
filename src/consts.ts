e29f8510bf6716a8e73027eff6fcf4049c525328
asninee <nine96as@proton.me>
2024-12-26 13:43:33 +0000

    feat: add `consts.ts` file

---
+++ src/consts.ts
@@ -0,0 +1,44 @@
+export const PROJECTS = [
+  {
+    name: 'asnine.me',
+    url: 'https://github.com/asninee/asnine.me',
+    desc: 'This site',
+    year: 2024,
+  },
+  {
+    name: 'codemaze_server',
+    url: 'https://github.com/asninee/codemaze_server',
+    desc: 'Backend for the Codemaze application',
+    year: 2024,
+  },
+  {
+    name: 'quizwiz_server',
+    url: 'https://github.com/asninee/quizwiz_server',
+    desc: 'Backend for the QuizWiz application',
+    year: 2023,
+  },
+  {
+    name: 'music_app',
+    url: 'https://github.com/asninee/music-app',
+    desc: 'An artist viewing catalogue',
+    year: 2023,
+  },
+  {
+    name: 'baloo-bot-final',
+    url: 'https://github.com/asninee/baloo-bot-final',
+    desc: 'A multi-purpose Discord bot',
+    year: 2023,
+  },
+]
+
+export const SOCIALS = [
+  {
+    name: 'github',
+    url: 'https://github.com/asninee',
+  },
+  {
+    name: 'linkedin',
+    url: 'https://www.linkedin.com/in/asnine',
+  },
+  { name: 'email', url: 'mailto:hey@asnine.me' },
+]
