cc869e220e7f74d4c40f0509ad84fb5d02e4aefa
asninee <nine96as@proton.me>
2025-02-09 18:51:25 +0000

    feat: enhance astro configuration with tailwindcss cleaner and sonda integration

--- astro.config.ts
+++ astro.config.ts
@@ -4,14 +4,33 @@ import playformCompress from '@playform/compress'
 import tailwindcss from '@tailwindcss/vite'
 import icon from 'astro-icon'
 import { defineConfig } from 'astro/config'
+import sonda from 'sonda/astro'
+import tailwindcssCleaner from 'vite-plugin-tailwindcss-cleaner'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://asnine.me',
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), tailwindcssCleaner()],
+    build: {
+      sourcemap: true,
+    },
   },
-  integrations: [mdx(), sitemap(), icon(), playformCompress()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    sonda({
+      server: true,
+    }),
+    icon(),
+    playformCompress({
+      CSS: {
+        lightningcss: {
+          unusedSymbols: [],
+        },
+      },
+    }),
+  ],
   markdown: {
     shikiConfig: {
       themes: {
